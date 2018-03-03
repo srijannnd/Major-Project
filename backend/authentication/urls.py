bc045fa24a0d9ae43d693ad57ce4833ac648b09d
srijan <srijannnd@gmail.com>
2018-03-03 10:24:08 +0530

    user-login-signup-api

---
+++ backend/authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from authentication.views import *
+
+app_name = "authentication"
+
+urlpatterns = [
+    path(r'users/', RegistrationAPIView.as_view()),
+    path(r'users/login/', LoginAPIView.as_view()),
+    path(r'user/', UserRetrieveUpdateAPIView.as_view()),
+]
