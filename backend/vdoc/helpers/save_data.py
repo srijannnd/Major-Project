c6e2150c222dc6f2bf41720c6323821b6a0bd871
srijan <srijannnd@gmail.com>
2018-03-18 23:14:06 +0530

    issue-description

--- backend/vdoc/helpers/save_data.py
+++ backend/vdoc/helpers/save_data.py
@@ -12,6 +12,7 @@ def save_data_helper():
     body_location_list_status = "Body Location List Not Updated"
     issue_list_status = "Issue List Not Updated"
     body_sub_location_list_status = "Body Sub Location List Not Updated"
+    issue_description_status = "Issue Descriptions Not Updated"
 
     # Save all Symptoms
     try:
@@ -25,7 +26,7 @@ def save_data_helper():
     try:
         for issue in issue_list:
             Issues.objects.create(id=issue['ID'], name=issue['Name'])
-        body_location_list_status = "Body Location List Updated Successfully"
+        issue_list_status = "Issue List Updated Successfully"
     except Exception as e:
         print(e)
 
@@ -33,7 +34,7 @@ def save_data_helper():
     try:
         for location in body_location_list:
             BodyLocations.objects.create(id=location['ID'], name=location['Name'])
-        issue_list_status = "Issue List Updated Successfully"
+        body_location_list_status = "Body Location List Updated Successfully"
     except Exception as e:
         print(e)
 
@@ -51,7 +52,19 @@ def save_data_helper():
     except Exception as e:
         print(e)
 
+    # Save Issue Description
+    try:
+        issues = Issues.objects.all()
+        for issue in issues:
+            if issue.description is None:
+                issue.description = obj.loadIssueInfo(issue.id)
+                issue.save()
+        issue_description_status = "Issue Description Updated Successfully"
+    except Exception as e:
+        print(e)
+
     return {'symptom_list_status': symptom_list_status,
             'body_location_list_status': body_location_list_status,
             'issue_list_status': issue_list_status,
-            'body_sub_location_list_status': body_sub_location_list_status}
+            'body_sub_location_list_status': body_sub_location_list_status,
+            'issue_description_status': issue_description_status}
