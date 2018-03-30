2d951804fa3f3dc2e5af36c9497ae54af45500d4
srijan <srijannnd@gmail.com>
2018-03-30 23:01:45 +0530

    api_structure_changes

--- backend/vdoc/helpers/reportsList.py
+++ backend/vdoc/helpers/reportsList.py
@@ -1,5 +1,5 @@
 from vdoc.models import Report, Symptoms, Issues
-
+from django.db.models import F
 
 def reports_list(user_id):
     reports = Report.objects.filter(user=user_id).values()
@@ -7,7 +7,10 @@ def reports_list(user_id):
         report['symptoms'] = Symptoms.objects.filter(
             id__in=map(int, report['symptoms'].split('#'))).values('id', 'name')
         report['issues'] = Issues.objects.filter(
-            id__in=map(int, report['issues'].split('#'))).values()
+            id__in=map(int, report['issues'].split('#'))).annotate(info=F('description'),
+                                                                   ID=F('id')).values(
+            'ID', 'info'
+        )
         for issue in report['issues']:
-            issue['description'] = eval(issue['description'])
+            issue['info'] = eval(issue['info'])
     return reports
